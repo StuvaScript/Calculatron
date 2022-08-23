2604d60919f8f19b55c36c0a63b354668cc68d87
Stu <stumac1000@yahoo.com>
2022-08-23 13:15:07 -0700

    Start calculatron project
    Got carried away with the beginnings of the project and forgot to commit
    often! Built the html boilerplate and styled the calculator.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Calculatron
+
+I'm going to make a functioning calculator. I'm going to build a visual representation that is clickable that performs arithmetic depending on the operators chosen.
---
+++ javascript.js
@@ -0,0 +1,20 @@
+
+// Basic Math Operators
+
+function add(x, y) {
+    return x + y;
+}
+
+function sub(x, y) {
+    return x - y;
+}
+
+function mult(x, y) {
+    return x * y;
+}
+
+function div(x, y) {
+    return x / y;
+}
+
+function operate() // to be continued...
