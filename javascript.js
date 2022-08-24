ebe241d1b74092e1ee6f266ac9de7df3e2e8c0c1
Stu <stumac1000@yahoo.com>
2022-08-24 10:50:55 -0700

    Add event listener to numbers and display value

--- javascript.js
+++ javascript.js
@@ -21,6 +21,54 @@ function div(x, y) {
 
 const btn = document.querySelectorAll('.button');
 btn.forEach(btn => {btn.addEventListener('click', (e) => {
-    console.log(e);
+    clickValue = e.target.id;
+    console.log(clickValue + ' clickValue');
+    convertNumber(clickValue);
+    console.log(clickValue + ' post function');
+    display(clickValue);
+    });
 });
-});
+
+let clickValue;
+
+function convertNumber() {
+    switch (clickValue) {
+        case 'zero':
+            clickValue = 0;
+            break;
+        case 'one':
+            clickValue = 1;
+            break;
+        case 'two':
+            clickValue = 2;
+            break;  
+        case 'three':
+            clickValue = 3;
+            break;
+        case 'four':
+            clickValue = 4;
+            break;  
+        case 'five':
+            clickValue = 5;
+            break;
+        case 'six':
+            clickValue = 6;
+            break; 
+        case 'seven':
+            clickValue = 7;
+            break;
+        case 'eight':
+            clickValue = 8;
+            break;  
+        case 'nine':
+            clickValue = 9;
+            break;           
+    }
+}
+
+function display(numb) {
+    if (typeof numb == 'number') {
+        const display = document.querySelector('.display');
+        display.textContent += numb;
+    }
+}
