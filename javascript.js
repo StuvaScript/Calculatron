12ea949bae0813baf167747e10859d5a6e579677
Stu <stumac1000@yahoo.com>
2023-04-03 13:33:34 -0700

    Quick change

--- javascript.js
+++ javascript.js
@@ -1,299 +1,310 @@
+document.addEventListener("keydown", allActions);
 
-document.addEventListener('keydown', allActions);
+const btn = document.querySelectorAll(".button");
 
-const btn = document.querySelectorAll('.button');
+btn.forEach((btn) =>
+  btn.addEventListener("mousedown", () => {
+    btn.style.cssText = "filter: brightness(0.50);";
+  })
+);
+//f
 
-btn.forEach(btn => btn.addEventListener('mousedown', () => {
-    btn.style.cssText = 'filter: brightness(0.50);';
-}));
+btn.forEach((btn) =>
+  btn.addEventListener("mouseup", () => {
+    btn.style.cssText = "filter: brightness(auto);";
+  })
+);
 
-btn.forEach(btn => btn.addEventListener('mouseup', () => {
-    btn.style.cssText = 'filter: brightness(auto);';
-}));
-
-btn.forEach(btn => {btn.addEventListener('click', allActions)});
+btn.forEach((btn) => {
+  btn.addEventListener("click", allActions);
+});
 
 function allActions(e) {
-    console.log(e.type);
-    if (e.type == 'keydown') {
-        keyStroke(e.key);
-    } else if (e.type == 'click') {
-        clickValue = e.target.id;
-    }
+  console.log(e.type);
+  if (e.type == "keydown") {
+    keyStroke(e.key);
+  } else if (e.type == "click") {
+    clickValue = e.target.id;
+  }
 
-    convertNumber();
-    convertPeriod();
-    back(tally);
-    console.log(clickValue + ' click value');
-    if (typeof clickValue == 'number' || clickValue == '.') {
-        controlNum += clickValue;
-        if (controlNum == 0) {
-            controlNum = '';
-        }
-        tally += clickValue;
-        if (tally == 0) {
-            tally = '';
-        }
-        maxChars(tally);
-        if (maxChars(tally)) {
-            display();
-        }
+  convertNumber();
+  convertPeriod();
+  back(tally);
+  console.log(clickValue + " click value");
+  if (typeof clickValue == "number" || clickValue == ".") {
+    controlNum += clickValue;
+    if (controlNum == 0) {
+      controlNum = "";
     }
-    zeroDivide();
-    percent();
-    posNeg();
-    operate();
-    clear();
-    console.log(firstNum + ' first num');
-    console.log(controlNum + ' control num');
-    console.log(tally + ' tally');
-    console.log(maxChars(tally) + ' max chars tally');
-    console.log(sign + ' sign');
-    console.log(equalToggle + ' equal toggle');
-    console.log(periodToggle + ' period toggle');
-    console.log('***********');
+    tally += clickValue;
+    if (tally == 0) {
+      tally = "";
+    }
+    maxChars(tally);
+    if (maxChars(tally)) {
+      display();
+    }
+  }
+  zeroDivide();
+  percent();
+  posNeg();
+  operate();
+  clear();
+  console.log(firstNum + " first num");
+  console.log(controlNum + " control num");
+  console.log(tally + " tally");
+  console.log(maxChars(tally) + " max chars tally");
+  console.log(sign + " sign");
+  console.log(equalToggle + " equal toggle");
+  console.log(periodToggle + " period toggle");
+  console.log("***********");
 }
 
 let clickValue;
-let tally = '';
+let tally = "";
 let firstNum;
-let controlNum = '';
+let controlNum = "";
 let sign;
 let equalToggle = false;
 let periodToggle = false;
 
 function keyStroke(value) {
-    if (value >= 0 && value <= 9) {
-        clickValue = +value;
-    }    
-    switch (value) {
-        case 'Backspace':
-            clickValue = 'back';
-            break;
-        case '%':
-            clickValue = 'percent';
-            break;
-        case '/':
-            clickValue = 'divide';
-            break;
-        case '*':
-            clickValue = 'multiply';
-            break;
-        case '-':
-            clickValue = 'minus';
-            break;
-        case '+':
-            clickValue = 'plus';
-            break;
-        case 'Enter':
-            clickValue = 'equals';
-            break;
-        case '.':
-            clickValue = 'period';
-            break;
-        case 'Delete':
-            clickValue = 'clear';
-            break;
-    } 
+  if (value >= 0 && value <= 9) {
+    clickValue = +value;
+  }
+  switch (value) {
+    case "Backspace":
+      clickValue = "back";
+      break;
+    case "%":
+      clickValue = "percent";
+      break;
+    case "/":
+      clickValue = "divide";
+      break;
+    case "*":
+      clickValue = "multiply";
+      break;
+    case "-":
+      clickValue = "minus";
+      break;
+    case "+":
+      clickValue = "plus";
+      break;
+    case "Enter":
+      clickValue = "equals";
+      break;
+    case ".":
+      clickValue = "period";
+      break;
+    case "Delete":
+      clickValue = "clear";
+      break;
+  }
 }
 
 function convertNumber() {
-    switch (clickValue) {
-        case 'zero':
-            clickValue = 0;
-            break;
-        case 'one':
-            clickValue = 1;
-            break;
-        case 'two':
-            clickValue = 2;
-            break;
-        case 'three':
-            clickValue = 3;
-            break;
-        case 'four':
-            clickValue = 4;
-            break;
-        case 'five':
-            clickValue = 5;
-            break;
-        case 'six':
-            clickValue = 6;
-            break;
-        case 'seven':
-            clickValue = 7;
-            break;
-        case 'eight':
-            clickValue = 8;
-            break;
-        case 'nine':
-            clickValue = 9;
-            break;
-    }
+  switch (clickValue) {
+    case "zero":
+      clickValue = 0;
+      break;
+    case "one":
+      clickValue = 1;
+      break;
+    case "two":
+      clickValue = 2;
+      break;
+    case "three":
+      clickValue = 3;
+      break;
+    case "four":
+      clickValue = 4;
+      break;
+    case "five":
+      clickValue = 5;
+      break;
+    case "six":
+      clickValue = 6;
+      break;
+    case "seven":
+      clickValue = 7;
+      break;
+    case "eight":
+      clickValue = 8;
+      break;
+    case "nine":
+      clickValue = 9;
+      break;
+  }
 }
 
 function convertPeriod() {
-    if (clickValue == 'period' && periodToggle == false) {
-        clickValue = '.';
-        periodToggle = true;
-    } else if (clickValue == 'period' && periodToggle == true) {
-        clickValue = '';
-    }
+  if (clickValue == "period" && periodToggle == false) {
+    clickValue = ".";
+    periodToggle = true;
+  } else if (clickValue == "period" && periodToggle == true) {
+    clickValue = "";
+  }
 }
 
 function display() {
-    const display = document.querySelector('.display');
-    display.textContent = maxChars(tally);
+  const display = document.querySelector(".display");
+  display.textContent = maxChars(tally);
 }
 
 function maxChars(value) {
-    let str = value.toString().split('');
-    console.log(str + ' str');
-    if (str[0] == 0) {
-        str.shift();
-    }
+  let str = value.toString().split("");
+  console.log(str + " str");
+  if (str[0] == 0) {
+    str.shift();
+  }
 
-    if (str.length > 13) {
-        let thirteen = str.slice(0, 13);
-        let join = thirteen.join('');
-        return +join;
-    };
-    let join = str.join('');
-    console.log(join + ' join');
+  if (str.length > 13) {
+    let thirteen = str.slice(0, 13);
+    let join = thirteen.join("");
     return +join;
+  }
+  let join = str.join("");
+  console.log(join + " join");
+  return +join;
 }
 
 function operate() {
-    if (equalToggle == true && clickValue != 'equals') {
-        if (typeof clickValue == 'number') {
-            tally = '';
-            controlNum = '';
-            controlNum += clickValue;
-            if (controlNum == 0) {
-                controlNum = '';
-            }
-            tally += clickValue;
-            if (tally == 0) {
-                tally = '';
-            }
-            maxChars(tally);
-            if (maxChars(tally)) {
-                display();
-            }
-        }
-        firstNum = '';
-        equalToggle = false;
+  if (equalToggle == true && clickValue != "equals") {
+    if (typeof clickValue == "number") {
+      tally = "";
+      controlNum = "";
+      controlNum += clickValue;
+      if (controlNum == 0) {
+        controlNum = "";
+      }
+      tally += clickValue;
+      if (tally == 0) {
+        tally = "";
+      }
+      maxChars(tally);
+      if (maxChars(tally)) {
+        display();
+      }
+    }
+    firstNum = "";
+    equalToggle = false;
+  }
+
+  if (
+    clickValue == "plus" ||
+    clickValue == "minus" ||
+    clickValue == "multiply" ||
+    clickValue == "divide" ||
+    clickValue == "equals"
+  ) {
+    if (!tally) {
+      sign = clickValue;
+      return;
     }
 
-    if (clickValue == 'plus' || clickValue == 'minus' || clickValue == 'multiply' || clickValue == 'divide' || clickValue == 'equals') {
+    periodToggle = false;
 
-        if (!tally) {
-            sign = clickValue;
-            return;
-        }
-
-        periodToggle = false;
-
-        if (firstNum && clickValue != 'equals') {
-            if (sign == 'plus') {
-                tally = add(+firstNum, +tally);
-            }
-            if (sign == 'minus') {
-                tally = sub(+firstNum, +tally);
-            }
-            if (sign == 'multiply') {
-                tally = mult(+firstNum, +tally);
-            }
-            if (sign == 'divide') {
-                tally = div(+firstNum, +tally);
-            }
-            display();
-        }
-
-        if (clickValue != 'equals') {
-            firstNum = tally;
-            controlNum = '';
-            tally = '';
-            sign = '';
-            sign = clickValue;
-        } else if (clickValue == 'equals') {
-            equalToggle = true;
-            if (sign == 'plus') {
-                tally = add(+controlNum, +firstNum);
-            }
-            if (sign == 'minus') {
-                tally = sub(+firstNum, +controlNum);
-            }
-            if (sign == 'multiply') {
-                tally = mult(+controlNum, +firstNum);
-            }
-            if (sign == 'divide') {
-                tally = div(+firstNum, +controlNum);
-            }
-            firstNum = tally;
-            display();
-        }
+    if (firstNum && clickValue != "equals") {
+      if (sign == "plus") {
+        tally = add(+firstNum, +tally);
+      }
+      if (sign == "minus") {
+        tally = sub(+firstNum, +tally);
+      }
+      if (sign == "multiply") {
+        tally = mult(+firstNum, +tally);
+      }
+      if (sign == "divide") {
+        tally = div(+firstNum, +tally);
+      }
+      display();
     }
+
+    if (clickValue != "equals") {
+      firstNum = tally;
+      controlNum = "";
+      tally = "";
+      sign = "";
+      sign = clickValue;
+    } else if (clickValue == "equals") {
+      equalToggle = true;
+      if (sign == "plus") {
+        tally = add(+controlNum, +firstNum);
+      }
+      if (sign == "minus") {
+        tally = sub(+firstNum, +controlNum);
+      }
+      if (sign == "multiply") {
+        tally = mult(+controlNum, +firstNum);
+      }
+      if (sign == "divide") {
+        tally = div(+firstNum, +controlNum);
+      }
+      firstNum = tally;
+      display();
+    }
+  }
 }
 
 function clear() {
-    if (clickValue == 'clear') {
-        tally = '';
-        clickValue = '';
-        firstNum = '';
-        controlNum = '';
-        sign = '';
-        equalToggle = false;
-        periodToggle = false;
-        display();
-    }
+  if (clickValue == "clear") {
+    tally = "";
+    clickValue = "";
+    firstNum = "";
+    controlNum = "";
+    sign = "";
+    equalToggle = false;
+    periodToggle = false;
+    display();
+  }
 }
 
 function posNeg() {
-    if (clickValue == 'pos-neg') {
-        tally = tally * (-1);
-        controlNum = controlNum * (-1);
-        display();
-    }
+  if (clickValue == "pos-neg") {
+    tally = tally * -1;
+    controlNum = controlNum * -1;
+    display();
+  }
 }
 
 function percent() {
-    if (clickValue == 'percent') {
-        tally = tally * 0.01;
-        controlNum = controlNum * 0.01;
-        display();
-    }
+  if (clickValue == "percent") {
+    tally = tally * 0.01;
+    controlNum = controlNum * 0.01;
+    display();
+  }
 }
 
 function back(value) {
-    if (clickValue == 'back'){
-        let diff = value.toString().split('');
-        diff.pop();
-        tally = diff.join('');
-        controlNum = diff.join('');
-        display();
-    }
+  if (clickValue == "back") {
+    let diff = value.toString().split("");
+    diff.pop();
+    tally = diff.join("");
+    controlNum = diff.join("");
+    display();
+  }
 }
 
 function zeroDivide() {
-    if (sign == 'divide' && maxChars(tally) == 0 && clickValue == 'equals') {
-        const display = document.querySelector('.display');
-        return display.textContent = 'The earth is shattered!';
-    }
+  if (sign == "divide" && maxChars(tally) == 0 && clickValue == "equals") {
+    const display = document.querySelector(".display");
+    return (display.textContent = "The earth is shattered!");
+  }
 }
 
 function add(x, y) {
-    return x + y;
+  return x + y;
 }
 
 function sub(x, y) {
-    return x - y;
+  return x - y;
 }
 
 function mult(x, y) {
-    return x * y;
+  return x * y;
 }
 
 function div(x, y) {
-    return x / y;
+  return x / y;
 }
